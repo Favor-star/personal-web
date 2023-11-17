90bc45b9d849934aa90b1d032e2b5d64108bb66a
Favor-star <favoureliab@gmail.com>
2023-11-17 16:09:07 +0200

    Worked on the works section and halfway of the view work section

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const loader = document.getElementById("loader");
 const welcome = document.getElementById("welcome-main-div");
 welcome.style.display = "none";
 
-const navbar  = document.getElementById("navbar-main-div");
-navbar.style.display = "none"
+const navbar = document.getElementById("navbar-main-div");
+navbar.style.display = "none";
+
+const aboutMe = document.getElementById("about-me-main-div");
+aboutMe.style.display = "none";
+
+const works = document.getElementById("works-main-div");
+works.style.display = "none"
 
 window.addEventListener("load", () => {
   setTimeout(() => {
